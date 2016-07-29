7f0258bff50cbbf513c06cf531c300b55d7dcba7
Ayinde Clarke <aclarke@asce.org>
2016-07-29 15:28:34 -0400

    message update
    Signed-off-by: Ayinde Clarke <aclarke@asce.org>

--- js/extra.js
+++ js/extra.js
@@ -9,4 +9,4 @@ $( document ).ready(function() {
 });
 
 // 
-$('body').prepend('<div class="site-alert"><p>Online conference registration will be unavailable on Friday, July 29, 2016 at 6 p.m. EDT through Sunday, July 31, 2016 at 12:00 p.m. EDT.</p></div>');
+$('body').prepend('<div class="site-alert"><p>Online purchases and updates to personal profiles will be unavailable on Friday, July 29, 2016 at 6 p.m. EDT through Sunday, July 31, 2016 at 12:00 p.m. EDT.</p></div>');
