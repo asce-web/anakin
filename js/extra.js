03a213027fee9370b2a04907e2307c7b3205731f
Ayinde Clarke <aclarke@asce.org>
2016-07-31 20:46:50 -0400

    site message removal
    Signed-off-by: Ayinde Clarke <aclarke@asce.org>

--- js/extra.js
+++ js/extra.js
@@ -8,5 +8,7 @@ $( document ).ready(function() {
   });
 });
 
-// 
-$('body').prepend('<div class="site-alert"><p>Online purchases and updates to personal profiles will be unavailable on Friday, July 29, 2016 at 6 p.m. EDT through Sunday, July 31, 2016 at 12:00 p.m. EDT.</p></div>');
+// Personify Maintenance updates -- DC 7.29.16
+// also modified:
+// - features.less
+//$('body').prepend('<div class="site-alert"><p>Online purchases and updates to personal profiles will be unavailable on Friday, July 29, 2016 at 6 p.m. EDT through Sunday, July 31, 2016 at 12:00 p.m. EDT.</p></div>');
--- js/min/extra-min.js
+++ js/min/extra-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){var e="/event/2016/asce-week";$(function(){0===location.pathname.indexOf(e)&&$(".highlight-box-blue, .content-right .section").hide()})}),$("body").prepend('<div class="site-alert"><p>Online purchases and updates to personal profiles will be unavailable on Friday, July 29, 2016 at 6 p.m. EDT through Sunday, July 31, 2016 at 12:00 p.m. EDT.</p></div>');
+$(document).ready(function(){var e="/event/2016/asce-week";$(function(){0===location.pathname.indexOf(e)&&$(".highlight-box-blue, .content-right .section").hide()})});
