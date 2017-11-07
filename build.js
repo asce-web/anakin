d4a7dac6bc3452b1686d86701fd5abb8fb7f9903
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-06 23:57:42 -0500

    remove x-articleteaser custom element

--- build.js
+++ build.js
@@ -32,17 +32,6 @@ function compile(template) {
     //  */
     // data.foundation_blurb = `` // NOTE may need this later
     /**
-     * Articles for What’s Happening section.
-     * This array should have no more than 3 items.
-     * Each object in the array should have a title (string), url (string), image (string), and date (Date).
-     * @type {Array<{title:string, url:string, image:string, date:Date>}}
-     */
-    data.news = [
-      { title: 'ASCE’s New Infrastructure Report Card: Another D+, But Solutions Available', url: '#0', image: '', date: new Date('2017-03-09') },
-      { title: 'Emergency and Carryover Storage Project Earns OCEA'                        , url: '#0', image: '', date: new Date('2017-03-16') },
-      { title: 'Former Corps Chief Bostick Honored with OPAL Award'                        , url: '#0', image: '', date: new Date('2017-03-16') },
-    ]
-    /**
      * Stories for Member Stories section.
      * This array should have no more than 3 items.
      * Each object in the array should have a name (string), title (string), image (string), and quote (string).
