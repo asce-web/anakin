7c0ebf3b2a18ff2d6042b8e5435099a893ef25b4
Dae <ayinde.clarke@gmail.com>
2017-12-06 16:06:16 -0500

    remove personify message

--- js/extra.js
+++ js/extra.js
@@ -22,7 +22,7 @@ $(window).load(function(){
 // removed -- DC 7.29.17
 // ASCE Convention status message added -- DC 10.6.17
 // Personify down status message -- DC 12.01.17
-$('body').prepend('<div class="site-alert"><p><strong>Maintenance Notice</strong>: ASCE will be performing system updates December 5th from 8pm &ndash; 10pm EST. Online transactions, including payment processing, will be unavailable during this time period.</p></div>');
+// $('body').prepend('<div class="site-alert"><p><strong>Maintenance Notice</strong>: ASCE will be performing system updates December 5th from 8pm &ndash; 10pm EST. Online transactions, including payment processing, will be unavailable during this time period.</p></div>');
 
 
 // // Publications Maintenance updates -- DC 8.18.17
