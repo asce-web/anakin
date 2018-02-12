8b06d953828dc74151fbb50a8255af6876d97323
Chris Harvey (work) <charvey@asce.org>
2018-02-12 15:53:45 -0500

    dirty hack for CE Mag button, the little fucker

---
+++ proto/js/hacks.js
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// NOTE: This script contains hacks and bugfixes.
+// This code is dirty and unstable, and is in no way whatsoever worth cleaning up.
+// Do not use this code as an example of anything.
+// This script should not be relied upon. It may break at any time without warning. It may also cause unexpected things to happen.
+// Edit with caution.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll('#civil-engineering-magazine .c-LinkList__Link').forEach(function (link) {
+  link.querySelector('i.glyphicons-chevron-right').remove()
+  link.classList.add('c-Button')
+})
