c4d26accbcfa5dbcc8be4658a335d5d221c1b431
Chris Harvey (work) <charvey@asce.org>
2018-02-23 16:25:45 -0500

    add gulp task home:lessc-each

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const gulp = require('gulp')
 const less         = require('gulp-less')
@@ -13,6 +14,27 @@ gulp.task('home:compile', function (callback) {
     let contents = new Homepage(JSON.parse(data)).compile()
     fs.writeFile('./proto/home.html', contents, 'utf8', callback) // send callback here to maintain async dependency
   })
+})
+
+gulp.task('home:lessc-each', function () {
+  return gulp.src(path.resolve(__dirname, './proto/css/src/*.less'))
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(autoprefixer({
+      grid: true,
+    }))
+    .pipe(gulp.dest('./proto/css/dist/'))
+    .pipe(clean_css({
+      level: {
+        2: {
+          overrideProperties: false,
+          mergeMedia: false,
+          restructureRules: true,
+        },
+      },
+    }))
+    .pipe(sourcemaps.write('./')) // write to an external .map file
+    .pipe(gulp.dest('./proto/css/dist/'))
 })
 
 gulp.task('home:lessc', function () {
