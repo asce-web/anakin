0191e09581dc520c09dcf4dadbb999b37ca7f63f
Dae <ayinde.clarke@gmail.com>
2018-06-13 22:04:24 -0400

    remove power message.

--- js/extra.js
+++ js/extra.js
@@ -22,7 +22,7 @@ $(window).load(function(){
 // removed -- DC 7.29.17
 // ASCE Convention status message added -- DC 10.6.17
 // Personify down status message -- DC 12.01.17
-$('body').prepend('<div class="site-alert"><p>Due to power outages in the area, some website functions, including the purchasing of products, are currently unavailable.</p></div>');
+// $('body').prepend('<div class="site-alert"><p>Due to power outages in the area, some website functions, including the purchasing of products, are currently unavailable.</p></div>');
 
 
 
