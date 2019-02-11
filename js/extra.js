252035cc6ecdb088666d53b4e7cc670ac0a60434
Dae <ayinde.clarke@gmail.com>
2019-02-11 15:55:55 -0500

    site messages remove - staging

--- js/extra.js
+++ js/extra.js
@@ -13,7 +13,7 @@ $(window).load(function () {
   $('#header-icon-livechat a, #footer-icon-livechat a').prepend('<span class="icon-livechat"></span>');
 });
 
-$('body').prepend('<div class="site-alert"><p>Due to system upgrades, online transactions will not be available from <strong>Thursday, February 7 at 5 p.m. EST through Monday, February 11</strong>. This includes registrations, purchases and changes to member records. If you need immediate assistance, please contact Customer Service at 1-800-548-2723 or 703-295-6300 (Mon–Fri 9 a.m.–6 p.m. EST) or email <a href="mailto:customercare@asce.org">customercare@asce.org</a>. Thank you for your patience.</p></div>');
+// $('body').prepend('<div class="site-alert"><p>Due to system upgrades, online transactions will not be available from <strong>Thursday, February 7 at 5 p.m. EST through Monday, February 11</strong>. This includes registrations, purchases and changes to member records. If you need immediate assistance, please contact Customer Service at 1-800-548-2723 or 703-295-6300 (Mon–Fri 9 a.m.–6 p.m. EST) or email <a href="mailto:customercare@asce.org">customercare@asce.org</a>. Thank you for your patience.</p></div>');
 
 // Personify Maintenance updates -- DC 7.29.16
 // also modified:
