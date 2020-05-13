e70bf3bb5cb5f525a6951d52fd9514a7c89a2d44
Dae Clarke <ayinde.clarke@gmail.com>
2020-05-13 15:04:08 -0400

    color and positioning change

--- js/extra.js
+++ js/extra.js
@@ -17,7 +17,7 @@ $(window).load(function () {
 var pathArray = window.location.pathname.split('/');
 var secondLevelLocation = pathArray[1];
 if (secondLevelLocation === "continuing-education") {
-  $('body').append('<div class="secondary-alert"><p><strong>Attention ASCE Customers</strong> Due to ASCE\'s annual year - end inventory count, all Print Book, DVD, CD ROM and Video orders submitted after 1:00 pm EST, Monday, September 9th, will begin shipping on Monday, September 16th. E-books will continue to be available for purchase through the ASCE Bookstore.</p></div>');
+  $('body').prepend('<div class="secondary-alert"><p><strong>Attention ASCE Customers</strong> Due to ASCE\'s annual year - end inventory count, all Print Book, DVD, CD ROM and Video orders submitted after 1:00 pm EST, Monday, September 9th, will begin shipping on Monday, September 16th. E-books will continue to be available for purchase through the ASCE Bookstore.</p></div>');
 }
 
 $('body').prepend('<div class="site-alert"><p><a class="black-link" href="/covid-19">COVID-19 OUTBREAK: ASCE Event Cancellations and Updates</a></p></div>');
